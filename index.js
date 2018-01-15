fc820f6ee6b664f112e9ee93b6c5cd4bf248a954
Quinn C. Hoyer <quinn@themightyquinn.com>
2018-01-15 14:00:18 -0800

    More work on tests

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
+const deDupeEngineVendors = require('./lib/de-dupe-engine-vendor')
+  .deDupeEngineVendor;
 
 module.exports = {
   name: 'ember-cli-lazy-loading-engine-vendor-deduper',
-  postBuild(result) {},
+  postBuild: deDupeEngineVendors,
 };
--- test/post-build-test.js
+++ test/post-build-test.js
@@ -1,7 +1,25 @@
+const path = require('path');
 const assert = require('assert');
+const deDupeEngineVendor = require('../lib/de-dupe-engine-vendor')
+  .deDupeEngineVendor;
+
+function runScenario(scenario) {
+  const scenarioPath = path.join(
+    process.cwd(),
+    'test-fixtures',
+    scenario,
+    'dist'
+  );
+
+  console.log(scenarioPath);
+}
 
 describe('Post Build tests', function() {
   it('runs the test', function() {
     assert.ok(true);
+  });
+
+  it('Passes Scenario One', function() {
+    runScenario('one');
   });
 });
